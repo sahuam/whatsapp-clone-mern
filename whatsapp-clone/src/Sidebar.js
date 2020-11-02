f69ea649132e5cd21cd9c1bf36c5a06a13b82a4b
sahuam <amrit.sahu10.95@gmial.com>
2020-11-02 19:47:01 +0530

    side part is created

--- whatsapp-clone/src/Sidebar.js
+++ whatsapp-clone/src/Sidebar.js
@@ -1,33 +1,41 @@
+import { Avatar, IconButton } from "@material-ui/core";
+import { MoreVertRounded, SearchOutlined } from "@material-ui/icons";
 import React from "react";
 import "./Sidebar.css";
-import DonutLargeIcon from "@material-ui/icons/DonutLarge";
-import { IconButton, Avatar } from "@material-ui/core";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
 import ChatIcon from "@material-ui/icons/Chat";
-import { SearchOutlined } from "@material-ui/icons";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import SidebarChat from "./SidebarChat.js";
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar_header">
-        <div className="sidebar_headerRight">
-          <Avatar className="sidebar_icons_avatar" src="https://media-exp1.licdn.com/dms/image/C5603AQEyl7jeS76t7Q/profile-displayphoto-shrink_100_100/0?e=1609977600&v=beta&t=NSrcqAac2-3wf2_eoSLw5pycWKm8ouzVcNIY5QS5LW0" />
+        <div className="sidebar_left">
+          <Avatar />
+        </div>
+        <div className="sidebar_right">
           <IconButton>
-            <DonutLargeIcon className="sidebar_icons"/>
+            <DonutLargeIcon />
           </IconButton>
           <IconButton>
-            <ChatIcon className="sidebar_icons"/>
+            <ChatIcon />
           </IconButton>
           <IconButton>
-            <MoreVertIcon className="sidebar_icons"/>
+            <MoreVertRounded />
           </IconButton>
         </div>
-        <div className="sidebar_search">
-            <div className="sidebar_searchContainer">
-                <SearchOutline/>
-                <input placeholder="Search or start new chat" type="text"/>
-            </div>
+      </div>
+      <div className="sidebar_search">
+        <div className="sidebar_searchContainer">
+          <SearchOutlined />
+          <input type="text" placeholder="Search or start a new chat" />
         </div>
+      </div>
+      <div className="sidebar_chat">
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
       </div>
     </div>
   );
